866822930a57c6b52e210d8f9a6cb587aec0eb65
Ryan Lawson <ryan.lawson@v-eden.com>
2019-04-29 21:09:54 -0400

    Fixed movement logic. The order that the cells are shifted is important depending on the direction they're moving.

--- src/main.js
+++ src/main.js
@@ -7,6 +7,8 @@ Vue.component('my-cell', Cell);
 
 Vue.config.productionTip = false
 const indexes = [0,1,2,3];
+const r_indexes = [3,2,1,0];
+
 function getRandom(min,max){
   return Math.floor(Math.random() * max) + min;
 };
@@ -89,55 +91,86 @@ var Grid = {
       return {coords: new_coords, value: -1};
     }
   },
-  moveDirection(cellAt){
+  // Methods to scan the grid from corner to corner and exec the move method on each cell
+  topToBottom(cellAt){
+    // iterate top left to bottom right
     for(let r of indexes){
       for(let c of indexes){
-        let coords = {r,c};
-        if(Grid.valueAt(coords) == 0) continue;
-        if(cellAt(coords).value == -1) continue;
-        if(cellAt(coords).value == 0){
-          Grid.set(cellAt(coords).coords, Grid.valueAt(coords));
-          Grid.clear(coords);
-        }
-
-        if(Grid.valueAt(coords) >= 3){
-          if(cellAt(coords).value == Grid.valueAt(coords)){
-            // console.log(`${cellAt(coords).value} == ${Grid.valueAt(coords)}`);
-            Grid.set(cellAt(coords).coords, cellAt(coords).value * 2);
-            Grid.clear(coords);
-          }
-        }else if(Grid.valueAt(coords) == 2 && cellAt(coords).value == 1){
-          Grid.set(cellAt(coords).coords, 3);
-          Grid.clear(coords);
-        }else if(Grid.valueAt(coords) == 1 && cellAt(coords).value == 2){
-          // console.log(`${cellAt(coords).value} == 2 && ${Grid.valueAt(coords)} == 1)`);
-          Grid.set(cellAt(coords).coords, 3);
-          Grid.clear(coords);
-        }else{
-          // console.log(`No Valid Move: ${Grid.above(coords).value} == ${Grid.valueAt(coords)}`);
-        }
-
+        Grid.moveDirection(cellAt, {r,c});
       }
+    }
+  },
+  bottomToTop(cellAt){
+    // iterate top left to bottom right
+    for(let r of r_indexes){
+      for(let c of r_indexes){
+        Grid.moveDirection(cellAt, {r,c});
+      }
+    }
+  },
+  leftToRight(cellAt){
+    // iterate bottom left to top right
+    for(let r of r_indexes){
+      for(let c of indexes){
+        Grid.moveDirection(cellAt, {r,c});
+      }
+    }
+  },
+  rightToLeft(cellAt){
+    // iterate top left to bottom right
+    for(let r of indexes){
+      for(let c of r_indexes){
+        Grid.moveDirection(cellAt, {r,c});
+      }
+    }
+  },
+  moveDirection(cellAt, coords){
+    // cellAt is a is one of above, below, left, or right to return that cell
+    // relative to the provided coords.
+    // Grid.right({r: 0, c: 0}) = {coords: {r: 0, c: 1}, value: x}
+    // We then check if the cell at coords can be moved into the cellAt the desired direction
+    if(Grid.valueAt(coords) == 0) return;
+    if(cellAt(coords).value == -1) return;
+    if(cellAt(coords).value == 0){
+      Grid.set(cellAt(coords).coords, Grid.valueAt(coords));
+      Grid.clear(coords);
+    }
+
+    if(Grid.valueAt(coords) >= 3){
+      if(cellAt(coords).value == Grid.valueAt(coords)){
+        // console.log(`${cellAt(coords).value} == ${Grid.valueAt(coords)}`);
+        Grid.set(cellAt(coords).coords, cellAt(coords).value * 2);
+        Grid.clear(coords);
+      }
+    }else if(Grid.valueAt(coords) == 2 && cellAt(coords).value == 1){
+      Grid.set(cellAt(coords).coords, 3);
+      Grid.clear(coords);
+    }else if(Grid.valueAt(coords) == 1 && cellAt(coords).value == 2){
+      // console.log(`${cellAt(coords).value} == 2 && ${Grid.valueAt(coords)} == 1)`);
+      Grid.set(cellAt(coords).coords, 3);
+      Grid.clear(coords);
+    }else{
+      // console.log(`No Valid Move: ${Grid.above(coords).value} == ${Grid.valueAt(coords)}`);
     }
   },
   moveUp(){
     // console.log("moveUp");
-    Grid.moveDirection(Grid.above);
+    Grid.topToBottom(Grid.above);
     Grid.addNumberUp();
   },
   moveDown(){
     // console.log("moveDown");
-    Grid.moveDirection(Grid.below);
+    Grid.bottomToTop(Grid.below);
     Grid.addNumberDown();
   },
   moveLeft(){
     // console.log("moveLeft");
-    Grid.moveDirection(Grid.left);
+    Grid.leftToRight(Grid.left);
     Grid.addNumberLeft();
   },
   moveRight(){
     // console.log("moveRight");
-    Grid.moveDirection(Grid.right);
+    Grid.rightToLeft(Grid.right);
     Grid.addNumberRight();
   },
   addNumberUp(){
