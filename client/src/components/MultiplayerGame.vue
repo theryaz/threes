e19b2c8d73a7f58849b01af441b5a5837c575a36
Ryan Lawson <ryan.lawson@v-eden.com>
2019-05-13 22:36:18 -0400

    Template multiplayer in place

--- client/src/components/MultiplayerGame.vue
+++ client/src/components/MultiplayerGame.vue
@@ -8,14 +8,33 @@
       <div id="game-heading">
         <h3>MultiplayerGame ID: {{state.gameOptions.uuid}}</h3>
       </div>
-      <div id="game-grid">
-        <div id="game-a">
-          <Game :options="optionsGameA"></Game>
-        </div>
-        <div id="game-b">
-          <Game :options="optionsGameB"></Game>
+      <div v-if="state.uuid == null">
+        <div id="controls">
+          <div class="button">
+            <button v-on:click="hostGame" class="common red-primary">
+              Host Game
+            </button>
+          </div>
+          <div class="button">
+            <div>
+              <input type="text" id="id-input" class="common" placeholder="Game ID" v-model="joinGameId"/>
+            </div>
+            <button v-on:click="joinGame" class="common blue-primary">
+              Join Game
+            </button>
+          </div>
         </div>
       </div>
+      <template v-else>
+        <div id="game-grid">
+          <div id="game-a">
+            <Game :paused="!state.isConnected" :options="optionsGameA"></Game>
+          </div>
+          <div id="game-b">
+            <Game :paused="!state.isConnected" :options="optionsGameB"></Game>
+          </div>
+        </div>
+      </template>
     </template>
   </div>
 </template>
@@ -27,11 +46,12 @@ import * as MultiplayerGameMutationTypes from '../store/multiplayer-game/multipl
 export default {
   name: 'MultiplayerGame',
   mounted(){
-    console.log("MultiplayerGame Dispatch HOST_GAME", MultiplayerGameMutationTypes.HOST_GAME);
-    this.$store.dispatch(MultiplayerGameMutationTypes.HOST_GAME);
+    // console.log("MultiplayerGame Dispatch HOST_GAME", MultiplayerGameMutationTypes.HOST_GAME);
+    // this.$store.dispatch(MultiplayerGameMutationTypes.HOST_GAME);
   },
   data:() => {
     return{
+      joinGameId: null,
       optionsGameA: {
         isMultiplayer: true,
         isRemote: false,
@@ -48,11 +68,19 @@ export default {
     state(){
       return this.$store.state.multiplayerGameState;
     }
+  },
+  methods:{
+    hostGame(){
+      console.log("hostGame");
+    },
+    joinGame(){
+      console.log("joinGame", this.joinGameId);
+    },
   }
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 
 #game-grid{
   display: grid;
@@ -73,5 +101,27 @@ export default {
 #main-loader{
   padding: 4rem;
 }
+
+#controls{
+  display: grid;
+  grid-template-columns: 50% 50%;
+  grid-template-rows: 1 2;
+
+  #id-input-wrapper{
+    grid-row-start: 1;
+    grid-row-end: 1;
+    grid-column-start: 2;
+    grid-column-end: 2;
+    input{
+      font-size: 1.2rem;
+      padding: 10px;
+    }
+  }
+  div.button{
+    grid-row-start: 2;
+    grid-row-end: 2;
+  }
+}
+
 
 </style>
