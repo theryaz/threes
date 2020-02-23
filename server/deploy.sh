d7133160a6ecfdd7b8c3a94dd437795bd6ca0a65
Ryan Lawson <ryan.lawson@v-eden.com>
2020-02-23 14:35:15 -0500

    Fix missing fields

--- server/deploy.sh
+++ server/deploy.sh
@@ -4,6 +4,5 @@ npm run build
 
 $(aws ecr get-login --no-include-email --region us-east-1)
 
-docker build -t threes/server .
-docker tag threes/server:latest 192407005904.dkr.ecr.us-east-1.amazonaws.com/threes/server:latest
-docker push 192407005904.dkr.ecr.us-east-1.amazonaws.com/threes/server:latest
+docker build -t 192407005904.dkr.ecr.us-east-1.amazonaws.com/threes/server:$1 .
+docker push 192407005904.dkr.ecr.us-east-1.amazonaws.com/threes/server:$1
