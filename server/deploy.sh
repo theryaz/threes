327508dcd667640824e653768d5493640a077dde
Ryan Lawson <ryan.lawson@v-eden.com>
2020-08-06 22:52:09 -0400

    Fix deploy script

--- server/deploy.sh
+++ server/deploy.sh
@@ -2,7 +2,7 @@
 
 npm run build
 
-$(aws ecr get-login --no-include-email --region us-east-1)
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 351575273764.dkr.ecr.us-east-1.amazonaws.com
 
-docker build -t 192407005904.dkr.ecr.us-east-1.amazonaws.com/threes/server:$1 .
-docker push 192407005904.dkr.ecr.us-east-1.amazonaws.com/threes/server:$1
+docker build -t 351575273764.dkr.ecr.us-east-1.amazonaws.com/threes/server:$1 .
+docker push 351575273764.dkr.ecr.us-east-1.amazonaws.com/threes/server:$1
