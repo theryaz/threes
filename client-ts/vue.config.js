123dc4517609584973d8417a729bb38190061ee5
Ryan Lawson <ryan.lawson437@gmail.com>
2021-12-19 21:56:07 -0500

    fix: update eslint and vue config

--- client-ts/vue.config.js
+++ client-ts/vue.config.js
@@ -2,22 +2,6 @@ module.exports = {
   configureWebpack:{
     devServer:{
       disableHostCheck: true,
-      public: "local.threeswithfriends.com:80",
     },
   },
-  // https://github.com/vuetifyjs/vuetify/issues/7977
-  // css: {
-  //   loaderOptions: {
-  //     sass: {
-  //       data: '@import "src/scss/variables.scss"',
-  //     }
-  //   },
-  //   sourceMap: true
-  // },
-  // chainWebpack: config => {
-  //   ["vue-modules", "vue", "normal-modules", "normal"].forEach((match) => {
-  //     config.module.rule('scss').oneOf(match).use('sass-loader')
-  //         .tap(opt => Object.assign(opt, { data: `@import 'src/scss/variables.scss';` }))
-  //   });
-  // }
 };
